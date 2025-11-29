afb16353c86c7992a403dc24e3665215610cede4
eRozhkov <rogev1989@gmail.com>
2025-11-29 13:58:15 +0300

    Initial Commit

---
+++ FinManager_v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinManager_v2
+//
+//  Created by Evgeni Rozkov on 29.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinManager_v2/FinManager_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  FinManager_v2App.swift
+//  FinManager_v2
+//
+//  Created by Evgeni Rozkov on 29.11.25.
+//
+
+import SwiftUI
+
+@main
+struct FinManager_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
